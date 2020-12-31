de0f7ac6f0e77537987a7b3eed50601c3f7f87ed
UpperLeaf <tkdduq8260@gmail.com>
2020-12-31 17:36:40 +0900

    Add Review Service

--- project.sh
+++ project.sh
@@ -9,9 +9,12 @@ git clone https://github.com/UpperLeaf/WhichBook_User.git whichbook_user
 echo ">> git clone Book Service"
 git clone https://github.com/SeonghoJin/WhichBook_Book.git whichbook_book
 
+echo ">> git clone Review Service"
+git clone https://github.com/UpperLeaf/WhichBook_Review.git whichbook_review
 
 docker image rm whichbook_apigateway
 docker image rm whichbook_user_service
 docker image rm whichbook_book_service
+docker image rm whichbook_review_service
 
 docker-compose up -d
